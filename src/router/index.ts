e7d72e75a26738ca9319a19d47c6f892f462923b
Pattanachai_n <pondlala33@gmail.com>
2023-08-12 01:43:35 +0700

    2_11_2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
-import EventListView2 from '../views/EventListView2.vue'
 import AboutView from '../views/AboutView.vue'
-import StudentListView from '../views/StudentListView.vue'
-import EventDetailView from '../views/event/EventDetailView.vue'
-import EventEditView from '../views/event/EventEditView.vue'
-import EventRegisterView from '../views/event/EventRegisterView.vue'
-import EventLayoutView from '../views/event/EventLayoutView.vue'
-import NotFoundView from '../views/NotFoundView.vue'
+import EventOrganizerView from '../views/EventOrganizerView.vue'
+import StudentListView from "../views/StudentListView.vue"
+import EventDetailView from "../views/event/EventDetailView.vue"
+import EventEditView from "../views/event/EventEditView.vue"
+import EventRegisterView from "../views/event/EventRegisterView.vue"
+import EventLayoutView from "../views/event/EventLayoutView.vue"
+import NotFoundView from "../views/NotFoundView.vue"
 import NetworkErrorView from '../views/NetworkErrorView.vue'
 import NProgress from 'nprogress'
 import EventService from '@/services/EventService'
@@ -20,74 +20,66 @@ const router = createRouter({
       path: '/',
       name: 'event-list',
       component: EventListView,
-      props: (route) => ({
-        page: parseInt((route.query?.page as string) || '1'),
-        limit: parseInt((route.query?.limit as string) || '2')
-      })
-    },
-    {
-      path: '/student',
-      name: 'student-list',
-      component: StudentListView
-    },
-    {
-      path: '/event2',
-      name: 'event-list2',
-      component: EventListView2
+      props : (route) => ({page: parseInt(route.query?.page as string || '1'),limit: parseInt(route.query?.limit as string || '2')})
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: AboutView
     },
-
     {
-      path: '/event/:id',
-      name: 'event-layout',
-      component: EventLayoutView,
-      // props: true,
+      path: '/organizer',
+      name: 'organizer',
+      component: EventOrganizerView
+    },
+    {
+      path: '/student',
+      name: 'student',
+      component: StudentListView
+    },
+    {
+      path : '/event/:id',
+      name : 'event-layout',
+      component : EventLayoutView,
       beforeEnter: (to) => {
         const id: number = parseInt(to.params.id as string)
         const eventStore = useEventStore()
         return EventService.getEventById(id)
-          .then((response) => {
-            eventStore.setEvent(response.data)
-          })
-          .catch(() => {
-            if (error.response && error.response.status === 404) {
-              return {
-                name: '404-resource',
-                params: { resource: 'event' }
-              }
-            } else {
-              return { name: 'network-error' }
+        .then((response) => {
+          eventStore.setEvent(response.data)
+        }).catch((error) => {
+          if (error.response && error.response.status === 404) {
+            return {
+              name : '404-resource'
+              ,params: { resource : 'event'}
             }
-          })
+          } else {
+            return { name : 'network-error'}
+          }
+        })
       },
-
-      children: [
+      children : [
         {
           path: '',
           name: 'event-detail',
           component: EventDetailView,
-          props: true
         },
         {
           path: 'edit',
           name: 'event-edit',
-          // props: true,
-          component: EventEditView
+          component: EventEditView,
         },
         {
           path: 'register',
           name: 'event-register',
-          // props: true,
-          component: EventRegisterView
+          component: EventRegisterView,
         }
       ]
+    },
+    {
+      path : '/:catchAll(.*)'
+    , name : 'not-found'
+    ,component : NotFoundView
     },
     {
       path: '/404/:resource',
@@ -96,27 +88,23 @@ const router = createRouter({
       props: true
     },
     {
-      path: '/:catchAll(.*)',
-      name: 'not-found',
-      component: NotFoundView
-    },
-    {
-      path: '/network-error',
-      name: 'network-error',
-      component: NetworkErrorView
+      path : '/network-error',
+      name : 'network-error',
+      component : NetworkErrorView
     }
+
   ],
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition
     } else {
-      return { top: 0 }
+      return { top : 0}
     }
   }
 })
 
 router.beforeEach(() => {
-  NProgress.start()
+NProgress.start()
 })
 
 router.afterEach(() => {
