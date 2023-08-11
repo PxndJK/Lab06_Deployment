4de922668e3a537c33ca0794a2c1d93fd7321cc4
Pattanachai_n <pondlala33@gmail.com>
2023-08-12 01:54:00 +0700

    2.11_3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { useMessageStore } from '@/stores/message'
-import { storeToRefs } from 'pinia'
+import { useMessageStore } from './stores/message';
+import { storeToRefs } from 'pinia';
+
 const store = useMessageStore()
 const { message } = storeToRefs(store)
+
 </script>
 
 <template>
@@ -14,27 +15,11 @@ const { message } = storeToRefs(store)
       <h4 class="text-lg">{{ message }}</h4>
     </div>
     <nav class="p-8">
-      <RouterLink :to="{ name: 'event-list' }" class="font-bold text-gray-700 hover:text-green-500">
-        Home
-      </RouterLink>
-      <RouterLink
-        :to="{ name: 'event-list2' }"
-        class="font-bold text-gray-700 hover:text-green-500"
-      >
-        Home2
-      </RouterLink>
-      <RouterLink
-        :to="{ name: 'student-list' }"
-        class="font-bold text-gray-700 hover:text-green-500"
-      >
-        Student
-      </RouterLink>
-      <RouterLink :to="{ name: 'about' }" class="font-bold text-gray-700 hover:text-green-500">
-        About
-      </RouterLink>
+      <RouterLink :to="{ name: 'event-list' }" class="font-bold text-gray-700 hover:text-green-500">Home</RouterLink>
+      <RouterLink :to="{ name: 'about' }" class="font-bold text-gray-700 hover:text-green-500">About</RouterLink>
+      
     </nav>
   </header>
-
   <RouterView />
 </template>
 
@@ -42,42 +27,18 @@ const { message } = storeToRefs(store)
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-font-smoothing: grayscale;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-/* nav {
-  padding: 30px;
-} */
 
-/* nav a {
-  font-weight: bold;
-  color: #2c3e50;
-} */
-
-/* nav a.router-link-exact-active {
-  color: #42b983;
-} */
-
-/* header {
-  line-height: 1.5;
-  max-height: 100vh;
-} */
-
-/* .logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-h4 {
-  font-size: 20px;
-} */
 
 @keyframes yellowfade {
   from {
     background: yellow;
   }
+
   to {
     background: transparent;
   }
